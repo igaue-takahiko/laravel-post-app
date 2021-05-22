52c12a98549e6a30e44c73be716b8d7f29debc01
igaue-takahhiko <hasewo.2610@gmail.com>
2021-05-22 22:46:27 +0900

    implement auth function

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -12,8 +12,8 @@
   <title>Post App</title>
 </head>
 
-<body class="bg-gray-100">
-    <nav class="p-6 bg-white flex justify-between">
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-3">
         <ul class="flex items-center">
             <li>
                 <a href="/" class="p-3">Home</a>
@@ -26,18 +26,25 @@
             </li>
         </ul>
         <ul class="flex items-center">
-            <li>
-                <a href="/" class="p-3">Igaue Takahiko</a>
-            </li>
-            <li>
-                <a href="/" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="/" class="p-3">Register</a>
-            </li>
-            <li>
-                <a href="/" class="p-3">Logout</a>
-            </li>
+            @auth
+                <li>
+                    <a href="/" class="p-3">Igaue Takahiko</a>
+                </li>
+                <li>
+                    <form action="{{ route('logout') }}" method="POST" class="inline p-3">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
+            @endauth
+            @guest
+                <li>
+                    <a href="{{ route('login') }}" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
         </ul>
     </nav>
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            home
+            dashboard
         </div>
     </div>
 @endsection
